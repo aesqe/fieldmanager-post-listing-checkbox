7d9560692d4c7c3c0e3b71a7b5c116b9243133c7
aesqe <aesqe@skyphe.org>
2015-04-12 21:21:01 +0200

    ... #2

--- fieldmanager-postlist-checkbox.php
+++ fieldmanager-postlist-checkbox.php
@@ -1,14 +1,16 @@
 <?php
 /*
 Plugin Name: Fieldmanager Post Listing Column Checkbox
+Plugin URi: https://github.com/aesqe/wordpress-fieldmanager-post-listing-checkbox
 Description: Adds toggleable checkboxes to the admin post listing screens programatically.
-Version: 0.1
+Version: 0.1.1
 Author: Bruno "Aesqe" Babic
 Author URI: http://skyphe.org
 */
 
-function fieldmanager_postlist_checkbox_init ()
-{
-	require_once('class-fieldmanager-postlist-checkbox.php');
+function fieldmanager_postlist_checkbox_after_setup_theme () {
+	if ( defined( 'FM_VERSION' ) ) {
+		require_once('class-fieldmanager-postlist-checkbox.php');
+	}
 }
-add_action('init', 'fieldmanager_postlist_checkbox_init');
+add_action('after_setup_theme', 'fieldmanager_postlist_checkbox_after_setup_theme');
